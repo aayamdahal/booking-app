ad1e73a6ae943237f81ff43fd621e285c93293cc
aayamdahal <getaayam@gmail.com>
2022-05-18 21:35:18 +0545

    add header styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import '../../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHotel } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
     <div className='topnav'>
-      <a
-        className='active'
-        href='#home'
-        style={{ display: 'flex', alignItems: 'center' }}
-      >
-        Booking App
-      </a>
+      <div className='logo-header'>
+        <a
+          className='active'
+          href='#home'
+          style={{ display: 'flex', alignItems: 'center' }}
+        >
+          <div
+            className='logo-header'
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'cneter',
+              
+            }}
+          >
+            <FontAwesomeIcon icon={faHotel} />
+            <span
+              style={{
+                marginLeft: '8px',
+
+               
+              }}
+            >
+              Booking App
+            </span>
+          </div>
+        </a>
+      </div>
       <div className='login-container'>
         <form>
-          <button type='submit' >Login</button>
+          <button type='submit'>Login</button>
           <button type='submit'>Register</button>
         </form>
       </div>
