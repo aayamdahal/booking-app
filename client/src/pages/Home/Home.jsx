f6c48af136e478f4447df5d4a97265687d3c0a91
aayamdahal <getaayam@gmail.com>
2022-05-20 10:32:25 +0545

    add header styles

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
+import Featured from '../../components/Featured/Featured';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className='homeContainer'>
+        <Featured />
+      </div>
     </div>
   );
 };
