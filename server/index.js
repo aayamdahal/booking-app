e41552002802ea3740d87dd246eab47835252384
aayamdahal <getaayam@gmail.com>
2022-05-22 19:08:15 +0545

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 const port = process.env.PORT || 8181;
 
-app.listen(port, () => {
-  console.log('server is running');
-});
+const start = async () => {
+  try {
+    // connectdb
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server spinning on ${port}`);
+    });
+    console.log('db connected');
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+start();
+  
