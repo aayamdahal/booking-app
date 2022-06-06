9fc60ecc6c4e9b8887f9b0feab7e58fc185f5d2b
aayamdahal <getaayam@gmail.com>
2022-06-06 12:42:06 +0545

    handled register and login

---
+++ server/controllers/auth.js
@@ -0,0 +1,42 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const createError = require('../utils/errors');
+
+const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(200).send('User registered successfully');
+  } catch (error) {
+    next(error);
+    console.log(error);
+    res.json(error);
+  }
+};
+const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, 'User not found'));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(400, 'Username or Password incorrect'));
+
+    res.status(200).send(`${user.username} is logged in`);
+  } catch (error) {
+    next(error);
+    console.log(error);
+    res.json(error);
+  }
+};
+
+module.exports = { register, login };
