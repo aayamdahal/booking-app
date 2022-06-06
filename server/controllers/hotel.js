a61d18b9e7f32e31535e8f946c0f69e32d23e178
aayamdahal <getaayam@gmail.com>
2022-06-06 09:45:33 +0545

    refactor routes in controllers

---
+++ server/controllers/hotel.js
@@ -0,0 +1,25 @@
+const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+    next(err);
+  }
+};
+const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
+};
+
+(module.exports = createHotel), updateHotel;
