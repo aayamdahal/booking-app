18110dec867ab4d5bae5a09a7afa97f40e355e63
aayamdahal <getaayam@gmail.com>
2022-06-13 09:38:45 +0545

    add jwt token verification

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const createError = require('../utils/errors');
-
+const jwt = require('jsonwebtoken');
 const register = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
@@ -30,8 +30,16 @@ const login = async (req, res, next) => {
     );
     if (!isPasswordCorrect)
       return next(createError(400, 'Username or Password incorrect'));
+    // password is correct we generate a token
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).json({ ...otherDetails });
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (error) {
     next(error);
     console.log(error);
